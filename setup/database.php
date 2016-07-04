d42dd213db500d525b49c1f4800b6500be7fe3f0
Yoann FUKS <yfuks@student.42.fr>
2016-07-04 22:05:00 +0200

    maybe it will work once

--- setup/database.php
+++ setup/database.php
@@ -1,6 +1,5 @@
 <?php
-$DB_DSN = "mysql:host=127.0.0.1";
-$DB_NAME = "camagru";
+$DB_DSN = "mysql:host=127.0.0.1;dbname=camagru";
 $DB_USER = "root";
 $DB_PASSWORD = "root";
 ?>
--- setup/setup.php
+++ setup/setup.php
@@ -1,17 +1,14 @@
 #!/usr/bin/php
 <?php
-include 'database.php'
+include 'database.php';
 
 try {
         $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $dbh->exec("CREATE DATABASE `'.$DB_NAME.'`;
-                CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
-                GRANT ALL ON `$db`.* TO '$DB_USER'@'localhost';
-                FLUSH PRIVILEGES;")
-        or die(echo $dbh->errorInfo());
-
+        $sql = "CREATE DATABASE camagru";
+        $dbh->exec($sql);
+        echo "Database created successfully<br>";
     } catch (PDOException $e) {
-        die(echo "DB ERROR: ".$e->getMessage());
+        echo "DB ERROR: ".$e->getMessage();
     }
 ?>
