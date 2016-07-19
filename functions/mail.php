2010aa5a42462aed07f445556fe2b336522bc24e
Yoann FUKS <yfuks@student.42.fr>
2016-07-19 18:42:43 +0200

    add first step of verification process

---
+++ functions/mail.php
@@ -0,0 +1,26 @@
+<?php
+
+function send_verification_email($toAddr, $toUsername, $token, $ip) {
+  $subject = "[CAMAGRU] - Email verification";
+
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+  $headers .= 'From: <yfuks@student.42.fr>' . "\r\n";
+
+  $message = '
+  <html>
+    <head>
+      <title>' . $subject . '</title>
+    </head>
+    <body>
+      Hello ' . $toUsername . ' </br>
+      To finalyze your subscribtion please click the link below </br>
+      <a href="http://' . $ip . '/verify.php?token=' . $token . '">Verify my email</a>
+    </body>
+  </html>
+  ';
+
+  mail($toAddr, $subject, $message, $headers);
+}
+
+?>
