902327c37dc01589d0808e2eb47e042edb3ff721
yfuks <yfuks@student.42.fr>
2016-08-02 18:58:48 +0200

    u

--- gallery.php
+++ gallery.php
@@ -13,11 +13,21 @@ session_start();
       <div class="body">
         <?php //if(isset($_SESSION['id'])) { ?>
         <div class="main">
-          <video autoplay="true" id="webcam">
-          </video>
+		  <div class="select">
+			<img class="thumbnail" src="img/cadre.png"></img>
+			<input id="cadre.png" type="radio" name="img" value="cadre.png">
+			<img class="thumbnail" src="img/cigarette.png"></img>
+			<input id="cigarette.png" type="radio" name="img" value="cigarette.png">
+			<img class="thumbnail" src="img/hat.png"></img>
+			<input id="hat.png" type="radio" name="img" value="hat.png">
+		  </div>
+          <video width="100%" autoplay="true" id="webcam"></video>
+		  <div class="capture"><img class="camera" src="img/camera.png"></img></div>
           <input type="file" id="take-picture" accept="image/*">
         </div>
-        <div class="side"></div>
+        <div class="side">
+			<div class="title">Montages</div>
+		</div>
         <?php //} else { ?>
           <!-- You need to connect to use the gallery -->
         <?php //} ?>
