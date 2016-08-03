f53ad37b10debb0aa9180b1f3aaa416bcfeda5c3
Yoann FUKS <yfuks@student.42.fr>
2016-08-03 13:47:53 +0200

    change position of filtre for each one

--- js/webcam.js
+++ js/webcam.js
@@ -10,7 +10,15 @@ if (navigator.getUserMedia) {
     button.onclick = function() {
       var image = new Image();
       image.addEventListener("load", function() {
-          canvas.getContext("2d").drawImage(image, 0, 0, 1024, 768, 0, 0, 640, 480);
+          var split = image.src.split("/");
+          var file = split[split.length - 1];
+          if (file === "cadre.png") {
+            canvas.getContext("2d").drawImage(image, 0, 0, 1024, 768, 0, 0, 640, 480);
+          } else if (file === "cigarette.png") {
+            canvas.getContext("2d").drawImage(image, 0, 0, 1024, 768, 100, 200, 240, 180);
+          } else {
+            canvas.getContext("2d").drawImage(image, 0, 0, 1024, 768, 180, 0, 240, 180);
+          }
       }, false);
       image.src = document.querySelector('input[name="img"]:checked').value; // Set source path
 
