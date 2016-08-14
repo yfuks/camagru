53cab1228e2d6e866c158f622838b9f36771beea
Yoann FUKS <yfuks@student.42.fr>
2016-08-14 16:13:55 +0200

    work on firefox

--- js/drop.js
+++ js/drop.js
@@ -3,14 +3,15 @@ var parent = document.getElementById("miniatures");
 
 for (var i=0; i < montage.length; i++) {
   montage[i].onclick = function(event) {
-    var pathToImg = event.srcElement.src;
+    console.log(event.target);
+    var pathToImg = (event.srcElement && event.srcElement.src) || (event.target && event.target.src);
     var srcTab = pathToImg.split('/');
     var src = srcTab[srcTab.length - 1];
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0) && xhr.responseText == "OK") {
-        parent.removeChild(event.srcElement);
+        parent.removeChild(event.srcElement || event.target);
       }
     };
     xhr.open("POST", "./forms/removemontage.php", true);
